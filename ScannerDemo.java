a64cde2ab4a0ae3160de146492f479450097c7e4
codewriter@vvit <107845549+Kartheek-G-39@users.noreply.github.com>
2022-09-15 09:17:30 +0530

    ScannerDemo.java

---
+++ ScannerDemo.java
@@ -0,0 +1,10 @@
+class DemoForScanner{
+public static void main(String args[]) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter Your Name");
+    String name=sc.nextLine();
+    System.out.println("Enter Your Age");
+    int age=sc.nextInt();
+        
+    }
+}
